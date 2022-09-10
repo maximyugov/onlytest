63fe09688cf7106bf3cf10f5a81129bf4949df64
Max <maxim.s.yugov@gmail.com>
2022-09-10 14:59:58 +0300

    Change registration template

--- views/register.tpl.php
+++ views/register.tpl.php
@@ -10,14 +10,22 @@
     <div>
         <h2>Регистрация</h2>
         <div>
-            <input type="login" name="login" placeholder="Логин">
+            <input type="text" name="login" placeholder="Логин">
+        </div>
+        <div>
+            <input type="text" name="email" placeholder="E-mail">
+        </div>
+        <div>
             <input type="password" name="password" placeholder="Пароль">
+        </div>
+        <div>
+            <input type="password" name="passwordcheck" placeholder="Повторите пароль">
         </div>
         <div>
             <button>Войти</button>
         </div>
         <div>
-            <a href="/register.php">Регистрация</a>
+            <a href="/login">Уже зарегистрированы?</a>
         </div>
     </div>
 </body>
