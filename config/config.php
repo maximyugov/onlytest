f954c427604368cf72468650e96a197cf732c9c5
Max <maxim.s.yugov@gmail.com>
2022-09-10 15:57:44 +0300

    Change config

--- config/config.php
+++ config/config.php
@@ -1,3 +1,7 @@
 <?php
 
-define('DB_DRIVER', 'sqlite');
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'onlytest');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
