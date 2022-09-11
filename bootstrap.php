ee4fa06a561abcf3714d8a958958a95acdcd1249
Max <maxim.s.yugov@gmail.com>
2022-09-11 19:43:24 +0300

    Delete unused class

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,6 @@
 require_once('classes/Auth.php');
 require_once('classes/Db.php');
 require_once('classes/Router.php');
-require_once('classes/Session.php');
 require_once('classes/User.php');
 require_once('classes/UserController.php');
 require_once('classes/View.php');
