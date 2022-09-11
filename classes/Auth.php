faef366a317504610cd00b843dd8d799acb75bea
Max <maxim.s.yugov@gmail.com>
2022-09-11 20:01:20 +0300

    Clean code

--- classes/Auth.php
+++ classes/Auth.php
@@ -6,7 +6,6 @@ class Auth
 {
     public static function login(User $user)
     {
-        //find user and return true or false
         $db = new Db();
 
         $authUser = $db->verifyUser($user);
