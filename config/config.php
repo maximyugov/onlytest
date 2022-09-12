5d2f5c456cb29835201d3f63dab01c0d0e803dc9
chulkov <chulkov@sibvaleo.com>
2022-09-12 18:37:58 +0700

    CR

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_DRIVER', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'onlytest');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
--- helpers/helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists('redirect')) {
-    function redirect(string $url) {
-        $basepath = 'http://' . $_SERVER['SERVER_NAME'];
-        header("Location: " . $basepath . $url);
-    }
-}
